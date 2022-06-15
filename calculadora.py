cf49774033ca5fc9714241226da3b7ed8c59db65
Jones Sabino <jones.sabino@gmail.com>
2022-06-15 18:53:01 -0300

    - renomeei arquivo - add func subt - add func mensagem

---
+++ calculadora.py
@@ -0,0 +1,11 @@
+def soma(a: int, b: int) -> int:
+    return a + b
+
+def subtracao(a: int, b: int) -> int:
+    return a - b
+
+def mensagem() -> str:
+    return "Olá, mundo!"
+
+if __name__ == "__main__":
+    soma()
